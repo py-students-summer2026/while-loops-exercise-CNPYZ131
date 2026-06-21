589b00cedfc0ee8a7b452bb21ba84e576736e4e2
CNPYZ131 <chenyuzhang05@gmail.com>
2026-06-21 09:12:14 -0400

    finished hw

---
+++ for_loop_version.py
@@ -0,0 +1,26 @@
+def get_starting_number():
+    while True:
+        try:
+            num = int(input("How many bottles of beer on the wall?"))
+            if num >= 1:
+                return num
+        except ValueError:
+            pass
+
+
+def bottle_text(num):
+    if num == 1:
+        return "1 bottle"
+    return f"{num} bottles"
+
+def sing(starting_bottles):
+    for bottles in range(starting_bottles, 0, -1):
+        print(f"{bottle_text(bottles)} of beer on the wall, {bottle_text(bottles)} of beer.")
+
+        if bottles == 1:
+            print("Take it down, pass it around, no more bottles of beer on the wall!")
+        else:
+            print(f"Take one down, pass it around, {bottle_text(bottles - 1)} of beer on the wall.")
+        
+        if bottles != 1:
+            print()
